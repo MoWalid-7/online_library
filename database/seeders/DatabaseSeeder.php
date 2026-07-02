8b16c85467b5b756444a147c43b35d2256cd7614
Mohamed Walid <mo7mdw3lid@gmail.com>
2026-07-02 14:08:22 +0300

    chore: hash admin user password in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@online-library.com',
-            'password' => 'password',
+            'password' => Hash::make('password'),
             'role' => 'admin',
         ]);
     }
